f731b479c04aeac2ca5d11edd1be612358382618
Raquel Ortega <rop13@alu.ua.es>
2022-07-01 11:00:58 +0200

    Arreglado error recargar formulario

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -19,6 +19,8 @@ export class AppComponent implements OnInit, AfterViewInit{
   HandsEnum = handEnum;
 
   test: any;
+  testPerson: any;
+  testHouse: any;
   title = 'Example form';
   labels: Map<string,string> = new Map();
   hidden: string[] = [];
@@ -26,18 +28,41 @@ export class AppComponent implements OnInit, AfterViewInit{
   theme: string = "custom";
 
   ngOnInit(): void { 
-    this.test = {person: {name:"Raquel", surnames: {s1: "Ortega", s2: "Pérez"}}, age: 22, eyes: this.EyesEnum['Dark brown'], hands: this.HandsEnum['Right-handed']}
+    this.generateObject(1);
+    this.generateObject(2);
+    this.test = this.testPerson
+  }
+
+  generateObject(obj: number){
+    if(obj == 1){
+      this.testPerson = {person: {name:"Raquel", surnames: {s1: "Ortega", s2: "Pérez"}}, age: 22, eyes: this.EyesEnum['Dark brown'], hands: this.HandsEnum['Right-handed']}
     
-    this.enums = new Map().set("eyes",eyesEnum).set("hands", handEnum)
+      this.enums = new Map().set("eyes",eyesEnum).set("hands", handEnum)
 
-    this.hidden = ["hands"];
+      this.hidden = ["hands"];
 
-    this.labels = new Map().set("name", "Name")
-                          .set("s1", "First surname")
-                          .set("s2","Second surname")
-                          .set("age", "Age")
-                          .set("eyes", "Eye color")
-                          .set("hands", "Dominant hand")
+      this.labels = new Map().set("name", "Name")
+                            .set("s1", "First surname")
+                            .set("s2","Second surname")
+                            .set("age", "Age")
+                            .set("eyes", "Eye color")
+                            .set("hands", "Dominant hand")
+    }
+    else if (obj == 2){
+      this.testHouse = {house: "Bungalow", windows: 5, occupied: true}
+      this.labels = new Map().set("house", "Type of house")
+                            .set("windows", "Number of windows")
+                            .set("occupied","Is ocuppied?")
+    }
+  }
+
+  changeForm(selected: number){
+    if(selected == 1){
+      this.test = this.testPerson;
+    } 
+    else if(selected == 2){
+      this.test = this.testHouse;
+    }
   }
 
   onChange(e: any) {
